ff663d9ee9fd3f2bdc2daa29eecd6c36efd9b61d
Adam Venturella <aventurella@blitzagency.com>
2015-03-29 18:53:51 -0700

    adds prototype alpha of a paged menu controller and MenuScrollView

---
+++ BUILT/iOS/MenuScrollView.cs
@@ -0,0 +1,377 @@
+﻿#if __IOS__
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UIKit;
+using Foundation;
+using CoreGraphics;
+
+namespace BUILT.iOS
+{
+    public class CV: UIView
+    {
+        public CV(CGRect frame): base(frame)
+        {
+        
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            var foo = 1;
+        }
+
+        public override void UpdateConstraints()
+        {
+            base.UpdateConstraints();
+            var foo = 1;
+        }
+    }
+    [Register("MenuScrollView")]
+    public class MenuScrollView : UIView
+    {
+        public int Padding { get; set; }
+        public UIScrollView ScrollView { get; set;}
+        public UIView ContentView { get; set;}
+
+
+        NSLayoutConstraint ContentViewWidth { get; set; }
+        List<UIView> queue { get; set;}
+        CGSize lastSize { get; set; }
+
+        public MenuScrollView (IntPtr handle) : base(handle)
+        {
+
+        }
+
+        [Export("initWithCoder:")]
+        public MenuScrollView (NSCoder coder) : base(coder)
+        {
+            Initialize();
+        }
+
+        protected void Initialize()
+        {
+            queue = new List<UIView> ();
+            Padding = 50;
+
+            ScrollView = new UIScrollView (CGRect.Empty);
+            ContentView = new CV (CGRect.Empty);
+            ContentView.BackgroundColor = UIColor.Red;
+
+            ScrollView.TranslatesAutoresizingMaskIntoConstraints = false;
+            ContentView.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            configureScrollView(ScrollView);
+
+            AddSubview(ScrollView);
+            ScrollView.AddSubview(ContentView);
+
+            applyInitialConstraints();
+        }
+
+        public void AddView(UIView view)
+        {
+            view.TranslatesAutoresizingMaskIntoConstraints = false;
+            queue.Add(view);
+
+            SetNeedsUpdateConstraints();
+            SetNeedsLayout();
+        }
+
+        protected virtual void configureScrollView(UIScrollView scrollView)
+        {
+            scrollView.Bounces = false;
+            scrollView.PagingEnabled = false;
+            scrollView.ShowsVerticalScrollIndicator = true;
+            scrollView.ShowsHorizontalScrollIndicator = true;
+        }
+
+        protected void applyInitialConstraints()
+        {
+            applyScrollViewConstraints();
+            applyContentViewConstraints();
+        }
+
+        protected void applyContentViewConstraints()
+        {
+            var t1 = NSLayoutConstraint.Create(
+                view1: ContentView,
+                attribute1: NSLayoutAttribute.Top,
+                relation: NSLayoutRelation.Equal,
+                view2: this,
+                attribute2: NSLayoutAttribute.Top,
+                constant: 0,
+                multiplier: 1
+            );
+
+            var b1 = NSLayoutConstraint.Create(
+                view1: ContentView,
+                attribute1: NSLayoutAttribute.Bottom,
+                relation: NSLayoutRelation.Equal,
+                view2: this,
+                attribute2: NSLayoutAttribute.Bottom,
+                constant: 0,
+                multiplier: 1
+            );
+               
+
+            AddConstraint(t1);
+            AddConstraint(b1);
+
+//            var h1 = NSLayoutConstraint.Create(
+//                                view1: ContentView,
+//                                attribute1: NSLayoutAttribute.Height,
+//                                relation: NSLayoutRelation.Equal,
+//                                view2: this,
+//                attribute2: NSLayoutAttribute.Height,
+//                                constant: 0,
+//                                multiplier: 1
+//                            );
+
+//            AddConstraint(h1);
+
+
+        }
+
+        protected void applyScrollViewConstraints()
+        {
+            var names = NSDictionary.FromObjectsAndKeys(
+                new NSObject[] { ScrollView },
+                new NSObject[] { new NSString ("ScrollView") }
+            );
+
+            var h1 = NSLayoutConstraint.FromVisualFormat("H:|[ScrollView]|", 0, null, names);
+            var v1 = NSLayoutConstraint.FromVisualFormat("V:|[ScrollView]|", 0, null, names);
+
+            AddConstraints(h1);
+            AddConstraints(v1);
+        }
+
+
+        public override void UpdateConstraints()
+        {
+            base.UpdateConstraints();
+
+//            if (queue.Count > 0)
+//                applyViewConstraints();
+
+            //updateContentViewConstraints();
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            if (queue.Count > 0)
+                applyViewConstraints();
+
+
+            // layout the menu items
+            ContentView.LayoutIfNeeded();
+
+            var lastView = ContentView.Subviews.Last();
+            var firstView = ContentView.Subviews.First();
+            var width = lastView.Bounds.Width + lastView.Frame.X;
+            var totalWidth = (nfloat)(width + (0.5 * Bounds.Width) - (lastView.Bounds.Width / 2));
+            var size = new CGSize (totalWidth, Bounds.Height);
+
+            if(ContentViewWidth != null)
+                RemoveConstraint(ContentViewWidth);
+            
+            ContentViewWidth = NSLayoutConstraint.Create(
+                view1: ContentView,
+                attribute1: NSLayoutAttribute.Width,
+                relation: NSLayoutRelation.Equal,
+                view2: null,
+                attribute2: NSLayoutAttribute.NoAttribute,
+                constant: size.Width,
+                multiplier: 1
+            );
+
+            AddConstraint(ContentViewWidth);
+            ScrollView.LayoutIfNeeded();
+            ScrollView.ContentSize = size;
+        }
+
+        protected void updateContentViewConstraints()
+        {
+            var children = ContentView.Subviews;
+            var count = children.Length == 0 ? 1 : children.Length;
+
+            ContentViewWidth = NSLayoutConstraint.Create(
+                view1: ContentView,
+                attribute1: NSLayoutAttribute.Width,
+                relation: NSLayoutRelation.Equal,
+                view2: this,
+                attribute2: NSLayoutAttribute.Width,
+                constant: 0,
+                multiplier: 1
+            );
+
+            AddConstraint(ContentViewWidth);
+        }
+
+        protected NSLayoutConstraint[] createInitialViewConstraints(UIView view)
+        {
+            // https://github.com/evgenyneu/center-vfl
+
+//            var names = NSDictionary.FromObjectsAndKeys(
+//                new NSObject[] { view, this },
+//                new NSObject[] { new NSString ("view"), new NSString ("superview") }
+//            );
+
+            // we are going to reset this constraint
+            // in LayoutSubviews to be relative to the ContentView
+            // we are setting it to the center of the containing view 
+            // so we can get the center coords.
+            var viewWidth = view.Bounds.Width;
+            var centerSuperview = (Bounds.Width / 2);
+            var left = centerSuperview - (viewWidth / 2);
+
+            var h1 = NSLayoutConstraint.Create(
+                view1: view,
+                attribute1: NSLayoutAttribute.Left,
+                relation: NSLayoutRelation.Equal,
+                view2: ContentView,
+                attribute2: NSLayoutAttribute.Left,
+                constant: left,
+                multiplier: 1
+            );
+
+//            var cv = NSLayoutConstraint.Create(
+//                view1: view,
+//                attribute1: NSLayoutAttribute.CenterY,
+//                relation: NSLayoutRelation.Equal,
+//                view2: this,
+//                attribute2: NSLayoutAttribute.CenterY,
+//                constant: 0,
+//                multiplier: 1
+//            );
+
+            return new NSLayoutConstraint[] {h1};
+        }
+
+        protected NSLayoutConstraint[] createWidthAndHeightConstraints(UIView view)
+        {
+            var w1 = NSLayoutConstraint.Create(
+                view1: view,
+                attribute1: NSLayoutAttribute.Width,
+                relation: NSLayoutRelation.Equal,
+                view2: null,
+                attribute2: NSLayoutAttribute.NoAttribute,
+                constant: view.Bounds.Width,
+                multiplier: 1
+            );
+
+            var h1 = NSLayoutConstraint.Create(
+                view1: view,
+                attribute1: NSLayoutAttribute.Height,
+                relation: NSLayoutRelation.Equal,
+                view2: null,
+                attribute2: NSLayoutAttribute.NoAttribute,
+                constant: view.Bounds.Height,
+                multiplier: 1
+            );
+
+            return new NSLayoutConstraint[] {w1, h1};
+        }
+
+        protected NSLayoutConstraint[] centerVerticallyConstraints(UIView view)
+        {
+//            var names = NSDictionary.FromObjectsAndKeys(
+//                new NSObject[] { view, this },
+//                new NSObject[] { new NSString ("view"), new NSString ("superview") }
+//            );
+//
+//            // center vertically
+//            var cv = NSLayoutConstraint.FromVisualFormat("H:[superview]-(<=1)-[view]", 
+//                NSLayoutFormatOptions.AlignAllCenterY, null, names);
+//
+//            return cv;
+
+            var cv = NSLayoutConstraint.Create(
+                         view1: view,
+                         attribute1: NSLayoutAttribute.CenterY,
+                         relation: NSLayoutRelation.Equal,
+                         view2: ContentView,
+                         attribute2: NSLayoutAttribute.CenterY,
+                         constant: 0,
+                         multiplier: 1
+                     );
+
+            return new NSLayoutConstraint[] {cv};
+        }
+
+        protected NSLayoutConstraint[] createViewConstraints(UIView previous, UIView view)
+        {
+            var names = NSDictionary.FromObjectsAndKeys(
+                new NSObject[] { previous, view },
+                new NSObject[] { new NSString ("previous"), new NSString ("view") }
+            );
+
+            var vfl = string.Format("H:[previous]-{0}-[view]", Padding);
+            var h1 = NSLayoutConstraint.FromVisualFormat(vfl, 0, null, names);
+
+            var c1 = createWidthAndHeightConstraints(view);
+
+            var result = new NSLayoutConstraint[h1.Length + c1.Length];
+            h1.CopyTo(result, 0);
+            c1.CopyTo(result, h1.Length);
+
+            return result;
+        }
+
+        protected void applyViewConstraints()
+        {
+            if (ContentView.Subviews.Length == 0)
+            {
+                for (var i = 0; i < queue.Count; i++)
+                {
+                    var view = queue [i];
+                    ContentView.AddSubview(view);
+
+                    if (i == 0)
+                    {
+                        // the inital view is centered against the parent view
+                        // this will be reset to be relative to the content view
+                        // in LayoutSubviews.
+                        ContentView.AddConstraints(createInitialViewConstraints(view));
+
+                        var cv = centerVerticallyConstraints(view);
+                        ContentView.AddConstraints(cv);
+
+                        var c1 = createWidthAndHeightConstraints(view);
+                        ContentView.AddConstraints(c1);
+                    }
+                    else
+                    {
+                        var previous = ContentView.Subviews [i - 1];
+
+                        var cv = centerVerticallyConstraints(view);
+                        ContentView.AddConstraints(cv);
+
+                        var c1 = createViewConstraints(previous, view);
+                        ContentView.AddConstraints(c1);
+                    }
+                }
+            }
+            else
+            {
+//                var baseIndex = ContentView.Subviews.Length - 1;
+//                for (var i = 0; i < queue.Count; i++)
+//                {
+//                    var view = queue [i];
+//                    ContentView.AddSubview(view);
+//
+//                    var previous = ContentView.Subviews [baseIndex + i];
+//                    ContentView.AddConstraints(createAdditionalHorizontalConstraints(previous, view));
+//                }
+            }
+
+           queue.Clear();
+        }
+    }
+}
+
+#endif
---
+++ BUILT/iOS/PagedMenuController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using CoreGraphics;
+
+
+namespace BUILT.iOS
+{
+    enum ScrollDirection {
+        Forward = 1,
+        Backward = -1,
+    }
+
+    [Register("PagedMenuController")]
+    public class PagedMenuController: UIViewController
+    {
+        PagedScrollView _pagedScrollView;
+        MenuScrollView _menuScrollView;
+
+        CGPoint _lastPagedOffset = new CGPoint(0, 0);
+        int _currentPageIndex = 0;
+        bool _pagedViewIsEndingDragging = false;
+        ScrollDirection _lastPagedDirection = ScrollDirection.Forward;
+
+        [Outlet]
+        public MenuScrollView MenuScrollView { 
+            get{ return _menuScrollView; } 
+            set{ 
+                if (_menuScrollView != null)
+                    _menuScrollView.ScrollView.WeakDelegate = null;
+                
+                _menuScrollView = value;
+                _menuScrollView.ScrollView.WeakDelegate = this;
+                _menuScrollView.ScrollView.ScrollEnabled = false;
+            }
+        }
+
+        [Outlet]
+        public PagedScrollView PagedScrollView {
+            get{ return _pagedScrollView; } 
+            set { 
+                if (_pagedScrollView != null)
+                    _pagedScrollView.ScrollView.WeakDelegate = null;
+
+                _pagedScrollView = value;
+                _pagedScrollView.ScrollView.WeakDelegate = this;
+            }
+        }
+
+        public PagedMenuController (PagedScrollView pagedScrollView = null, MenuScrollView menuScrollView = null)
+        {
+            if(pagedScrollView != null)
+                PagedScrollView = pagedScrollView;
+
+            if(menuScrollView != null)
+                MenuScrollView = menuScrollView;
+        }
+
+        [Export("scrollViewDidScroll:")]
+        public void scrollViewDidScroll(UIScrollView scrollView)
+        {
+            if (scrollView == PagedScrollView.ScrollView)
+                pagedScrollViewDidScroll(scrollView);
+            else if (scrollView == MenuScrollView.ScrollView)
+                menuScrollViewDidScroll(scrollView);
+        }
+
+        [Export("scrollViewWillEndDragging:withVelocity:targetContentOffset:")]
+        public void scrollViewWillEndDragging(UIScrollView scrollView, CGPoint velocity, ref CGPoint targetContentOffset)
+        {
+            if (scrollView == PagedScrollView.ScrollView)
+                pagedScrollViewWillEndDragging(scrollView, velocity, ref targetContentOffset);
+            else if (scrollView == MenuScrollView.ScrollView)
+                menuScrollViewWillEndDragging(scrollView, velocity, ref targetContentOffset);
+
+        }
+
+        [Export("scrollViewDidEndDecelerating:")]
+        public void scrollViewDidEndDecelerating(UIScrollView scrollView)
+        {
+            if (scrollView == PagedScrollView.ScrollView)
+                pagedScrollViewDidEndDecelerating(scrollView);
+            else if (scrollView == MenuScrollView.ScrollView)
+                menuScrollViewDidEndDecelerating(scrollView);
+        }
+
+        protected void pagedScrollViewWillEndDragging(UIScrollView scrollView, CGPoint velocity, ref CGPoint targetContentOffset)
+        {
+            _lastPagedOffset = targetContentOffset;
+            _pagedViewIsEndingDragging = true;
+
+            var currentView = MenuScrollView.ContentView.Subviews [_currentPageIndex];
+            var nextView = MenuScrollView.ContentView.Subviews [_currentPageIndex + (int)_lastPagedDirection];
+            var distance = (currentView.Bounds.Width / 2) + MenuScrollView.Padding + (nextView.Bounds.Width / 2);
+            var ratio = distance / PagedScrollView.Bounds.Width;
+            var delta = targetContentOffset.X * ratio;
+
+            var point = new CGPoint (delta, 0);
+
+            UIView.Animate(
+                duration: 0.35, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, 
+                animation: delegate{
+                    MenuScrollView.ScrollView.ContentOffset = point;
+            }, completion: null);
+
+            _currentPageIndex = (int)(targetContentOffset.X / PagedScrollView.Bounds.Width);
+
+            currentView.Alpha = (nfloat)0.5;
+            nextView.Alpha = 1;
+        }
+
+        protected void menuScrollViewWillEndDragging(UIScrollView scrollView, CGPoint velocity, ref CGPoint targetContentOffset)
+        {
+            Console.WriteLine("menuScrollViewWillEndDragging");
+        }
+
+        protected void pagedScrollViewDidEndDecelerating(UIScrollView scrollView)
+        {
+            _pagedViewIsEndingDragging = false;
+        }
+
+        protected void menuScrollViewDidEndDecelerating(UIScrollView scrollView)
+        {
+
+        }
+
+        protected void pagedScrollViewDidScroll(UIScrollView scrollView)
+        {
+            if (_pagedViewIsEndingDragging)
+                return;
+            
+            ScrollDirection direction = ScrollDirection.Forward;
+
+            if (scrollView.ContentOffset.X > _lastPagedOffset.X)
+                direction = ScrollDirection.Forward;
+            else if (scrollView.ContentOffset.X < _lastPagedOffset.X)
+                direction = ScrollDirection.Backward;
+
+            _lastPagedDirection = direction;
+            
+            var padding = MenuScrollView.Padding;
+            var currentView = MenuScrollView.ContentView.Subviews [_currentPageIndex];
+            var nextView = MenuScrollView.ContentView.Subviews [_currentPageIndex + (int)direction];
+            var distance = (currentView.Bounds.Width / 2) + padding + (nextView.Bounds.Width / 2);
+
+            var offset = _lastPagedOffset = scrollView.ContentOffset;
+            var ratio = distance / PagedScrollView.Bounds.Width;
+            var delta = offset.X * ratio;
+
+            MenuScrollView.ScrollView.ContentOffset = new CGPoint (delta, 0);
+            Console.WriteLine(delta);
+
+        }
+
+        protected void menuScrollViewDidScroll(UIScrollView scrollView)
+        {
+        
+        }
+    }
+}
+
